330e9e5cd8b260d33da4823b834b02067ea1fe9d
HexDee <lukmansaheed79@gmail.com>
2021-08-15 15:09:20 +0100

    added test file

--- contracts/EasySwap.sol
+++ contracts/EasySwap.sol
@@ -13,36 +13,36 @@ contract EasySwap {
     address public constant DAI = 0xc2118d4d90b274016cB7a54c03EF52E6c537D957;
     address public constant WETH9 = 0xc778417E063141139Fce010982780140Aa0cD5Ab;
     
-	function depositWithETH() external payable {
-    // Call swapExactETHForToken function
-        
-        address[] memory path = new address[](2);
-        path[0] = WETH9;
-        path[1] = DAI;
-        
-        IUniswapV2Router02(swapRouter).swapExactETHForTokens(0, path, address(this), block.timestamp);
-    }
-	
-    function deposit(uint256 _amountIn) external {
-        uint amountIn = _amountIn * 10 ** 18;
+	function swapWETHToDAI(uint256 amountIn) external {
 		// msg.sender must approve this contract to spend their WETH
 
         // Transfer the specified amount of WETH to this contract.
         TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountIn);
 
-        // Approve the router to spend DAI.
+        // Approve the router to spend WETH.
         TransferHelper.safeApprove(WETH9, swapRouter, amountIn);
-
+		
         address[] memory path = new address[](2);
         path[0] = WETH9;
         path[1] = DAI;
         
-        IUniswapV2Router02(swapRouter).swapExactTokensForTokens(amountIn, 0, path, address(this), block.timestamp);
+        IUniswapV2Router02(swapRouter).swapExactTokensForTokens(amountIn, 0, path, msg.sender, block.timestamp);
     }
 	
-	// Allows user to withdraw their stableCoins
-	function withdraw(uint256 _amount) external{
-		uint amount = _amount * 10 ** 18;
-		TransferHelper.safeTransfer(DAI, msg.sender, amount);
-	}
+	function swapDAIToWETH(uint256 amountIn) external {
+		// msg.sender must approve this contract to spend their DAI
+
+        // Transfer the specified amount of DAI to this contract.
+        TransferHelper.safeTransferFrom(DAI, msg.sender, address(this), amountIn);
+
+        // Approve the router to spend DAI.
+        TransferHelper.safeApprove(DAI, swapRouter, amountIn);
+		
+        address[] memory path = new address[](2);
+        path[0] = DAI;
+        path[1] = WETH9;
+        
+        IUniswapV2Router02(swapRouter).swapExactTokensForTokens(amountIn, 0, path, msg.sender, block.timestamp);
+    }
 }
+
